ff5e130a42e5165481777fef8e59293a926d7b59
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-02 12:37:59 +0100

    snack-4.1

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,16 +7,26 @@
 
 var listaNomi = ["giorgio", "franco", "samuele", "giulia"];
 var nomeUtente = prompt("scrivi il tuo nome e controllo se farti entrare");
-var validato = 0;
+var validato = false;
 
+
+// var validato = 0;
+
+
+// for (var i = 0; i < listaNomi.length; i++) {
+//   if (nomeUtente === listaNomi[i]) {
+//     console.log("ok entra");
+//     validato = 1;
+//   }
+// }
+//  verifico che il nome sia presente nell'array
 for (var i = 0; i < listaNomi.length; i++) {
-  if (nomeUtente === listaNomi[i]) {
-    console.log("ok entra");
-    validato = 1;
+  if ( listaNomi[i].toLowerCase() == nomeUtente.toLowerCase() ) {
+    validato = true;
   }
 }
 
-if (validato == 1) {
+if (validato == true) {
   document.getElementById('testo').innerHTML = "Puoi entrare";
 
 } else {
